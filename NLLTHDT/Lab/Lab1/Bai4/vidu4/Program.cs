71db09df44f19e56cb6c8a16dd5c89f35190fb8f
thientanhuynh189 <49064303+thientanhuynh189@users.noreply.github.com>
2020-03-29 23:45:15 -0700

    Add all file

---
+++ NLLTHDT/Lab/Lab1/Bai4/vidu4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace vidu4
+{
+    public class Phanso
+    {
+        private int tuso;
+        private int mauso;
+        public void nhap()
+        {
+            Console.WriteLine("Nhap tu so: ");
+            tuso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap mau so: ");
+            mauso = int.Parse(Console.ReadLine());
+        }
+        public void show()
+        {
+            Console.WriteLine("Phan so: {0}/{1}", tuso, mauso);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                var phanso = new Phanso();
+                phanso.nhap();
+                phanso.show();
+            
+        }
+    }
+}
